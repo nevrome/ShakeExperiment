39f1805a809545d42d02a3c7b1029965656c8772
Clemens Schmid <clemens@nevrome.de>
2021-11-30 15:21:23 +0100

    indentation

--- Shakefile1.hs
+++ Shakefile1.hs
@@ -9,24 +9,24 @@ import Development.Shake.Util
 main :: IO ()
 main = shakeArgs shakeOptions {shakeFiles = "_build"} $ do
 
-    want [ "output" </> "3D.html" ]
-    
-    "output" </> "3D.html" %> \out -> do
-        let script = "scripts" </> "C.R"
-            dataFiles = [
-                "intermediate" </> "dens_surface.RData", 
-                "intermediate" </> "colours.RData" 
-                ]
-        need $ script : dataFiles
-        cmd_ "Rscript" script
+  want [ "output" </> "3D.html" ]
+  
+  "output" </> "3D.html" %> \out -> do
+    let script = "scripts" </> "C.R"
+        dataFiles = [
+          "intermediate" </> "dens_surface.RData", 
+          "intermediate" </> "colours.RData" 
+          ]
+    need $ script : dataFiles
+    cmd_ "Rscript" script
 
-    "intermediate" </> "dens_surface.RData" %> \out -> do
-        let script = "scripts" </> "A.R"
-            dataFiles = [ "input" </> "raw_input.csv" ]
-        need $ script : dataFiles
-        cmd_ "Rscript" script
+  "intermediate" </> "dens_surface.RData" %> \out -> do
+    let script = "scripts" </> "A.R"
+        dataFiles = [ "input" </> "raw_input.csv" ]
+    need $ script : dataFiles
+    cmd_ "Rscript" script
 
-    "intermediate" </> "colours.RData" %> \out -> do
-        let script = "scripts" </> "B.R"
-        need [ script ]
-        cmd_ "Rscript" script
+  "intermediate" </> "colours.RData" %> \out -> do
+    let script = "scripts" </> "B.R"
+    need [ script ]
+    cmd_ "Rscript" script
